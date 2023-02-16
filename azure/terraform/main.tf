d7f0e8a29280124d49085a3dc2e9b1e24d3845b2
Umer Singhera <umer@ilmiya.com>
2023-02-16 19:55:30 +0500

    fix network issue

--- azure/terraform/main.tf
+++ azure/terraform/main.tf
@@ -46,13 +46,18 @@ resource "azurerm_databricks_workspace" "this" {
   location            = azurerm_resource_group.this.location
   sku                 = var.databricks_sku
 
-#   custom_parameters {
-#     virtual_network_id  = azurerm_virtual_network.this.id
-#     # public_subnet_name  = azurerm_subnet.public.name
-#     # private_subnet_name = azurerm_subnet.private.name
-#   }
+  custom_parameters {
+    virtual_network_id  = azurerm_virtual_network.this.id
+    public_subnet_name  = azurerm_subnet.public.name
+    private_subnet_name = azurerm_subnet.private.name
+    public_subnet_network_security_group_association_id = azurerm_subnet_network_security_group_association.public.id
+    private_subnet_network_security_group_association_id = azurerm_subnet_network_security_group_association.private.id
+  }
 
   depends_on = [
+    azurerm_virtual_network.this,
+    azurerm_subnet.public,
+    azurerm_subnet.private,
     azurerm_subnet_network_security_group_association.public,
     azurerm_subnet_network_security_group_association.private,
   ]
